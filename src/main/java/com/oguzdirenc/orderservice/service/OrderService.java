877add11975c9a8a905febf06e1dfb85f60fea02
oguzdirenc <oguzdirenc@hotmail.com>
2023-09-19 19:31:53 +0300

    Transactional annotation implemented

--- src/main/java/com/oguzdirenc/orderservice/service/OrderService.java
+++ src/main/java/com/oguzdirenc/orderservice/service/OrderService.java
@@ -7,12 +7,14 @@ import com.oguzdirenc.orderservice.model.OrderLineItems;
 import com.oguzdirenc.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
 
     private final OrderRepository orderRepository;
@@ -20,7 +22,7 @@ public class OrderService {
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
-
+        
         List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
                 .stream()
                 .map(this::mapToDto)
